665cd2bd64ad09a447f915d6d3f84c308e47c51a
Admin <admin@Admins-MacBook-Pro.local>
2023-01-07 22:12:38 +0000

    fil structure

---
+++ components/BottomTab.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BottomTab = () => {
+    return (
+        <View className="bg-white top-[700px] h-[80px]">
+            <Text>BottomTab</Text>
+        </View>
+    )
+}
+
+export default BottomTab
---
+++ screens/LoginScreen.js
@@ -0,0 +1,25 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import BottomTab from '../components/BottomTab';
+import Login from '../components/Login';
+
+
+const LoginScreen = () => {
+    const navigation = useNavigation()
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
+
+    return (
+        <SafeAreaView className="flex-1 bg-gray-200">
+            <Login />
+            <BottomTab />
+        </SafeAreaView>
+    )
+}
+
+export default LoginScreen
